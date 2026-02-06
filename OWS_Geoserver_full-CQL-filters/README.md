6234e7824d22e2d964c86717b000083ce7395f34
Henning Lorenz <henning@geolorenz.se>
2026-02-06 13:34:08 +0100

    feat: handling of KVP bounding box
    add handling of KVP boudning boxes by transformation to CQL-bounding box considering multiple geometries;
    bug fixing

--- OWS_Geoserver_full-CQL-filters/README.md
+++ OWS_Geoserver_full-CQL-filters/README.md
@@ -9,6 +9,8 @@ The EPOS data portal ([EPOS Platform Open Source](https://epos-eric.github.io/op
 
 => Other functionality of CQL-filters, as described in the [CQL reference](https://docs.geoserver.org/latest/en/user/filter/ecql_reference.html#filter-ecql-reference), is not supported but can be implemented in a similar way.
 
+=> The script also transforms WFS KVP-bounding boxes into CQL-filter bounding boxes while taking care of multiple geometries.
+
 ### Requirements:
 * A Geoserver WFS service that is configured to produce the desired service payload. \
 => Use EPOS-extended GeoJSON for best compatibility with EPOS Platform Open Source (Recommendation: Geoserver's [Features-Templating extension](https://docs.geoserver.org/main/en/user/community/features-templating/index.html))
@@ -17,4 +19,4 @@ The EPOS data portal ([EPOS Platform Open Source](https://epos-eric.github.io/op
 
 The service.php script expects a request from the data portal according to the example provided in [example.ttl](https://github.com/daoane/epos-services/edit/main/OWS_Geoserver_full-CQL-filters/example.ttl)). It deconstructs an incoming request, reformats its parts to comply with CQL-filter specifications, constructs the WFS query-URL, sends the request to Geoserver and forwards the payload of the reply to the client (the data portal).
 
-Initial service requests on the EPOS Platform (when activating a service) do not include filters. Users often do not use filters while exploring the data. Thus, a large part of the requests from the EPOS data portal do not include filters. To avoid unnecessary load on the server, it is recommended to configure the web server to relay queries without filters directly to Geoserver, bypassing the service.php script (e.g. with Apache mod_rewrite in the virtual host, e.g. RewriteCond "%{REQUEST_URI}" "!CQL_FILTER=").
+Initial service requests on the EPOS Platform (when activating a service) do not include filters. Users often do not use filters while exploring the data. Thus, a large part of the requests from the EPOS data portal do not include filters. To avoid unnecessary load on the server, it is recommended to configure the web server to relay queries without CQL-filters or KVP-bounding box directly to Geoserver, bypassing the service.php script (e.g. with Apache mod_rewrite in the virtual host, e.g. e.g. RewriteCond "%{REQUEST_URI}" "!CQL_FILTER" [NC] combined with RewriteCond "%{REQUEST_URI}" "!BBOX" [NC]).
--- OWS_Geoserver_full-CQL-filters/service.php
+++ OWS_Geoserver_full-CQL-filters/service.php
@@ -1,9 +1,11 @@
 <?php
 
 /**
- * USE SUITABLE SERVER PROXYING TO RELAY ALL REQUESTS WITHOUT ATTRIBUTE FILTERS
- * DIRECTLY TO THE GEOSERVER INSTANCE AND PROCESS ONLY QUERIES WITH FILTERS IN PHP
- * (Apache: mod_rewrite in the virtual host, e.g. RewriteCond "%{REQUEST_URI}" "!CQL_FILTER=")
+ * USE SUITABLE SERVER PROXYING TO RELAY ALL REQUESTS WITH CQL_FILTERS AND BBOX TO THIS SCRIPT,
+ * (Apache: mod_rewrite in the virtual host, e.g. RewriteCond "%{REQUEST_URI}" "CQL_FILTER|BBOX" [NC])
+ * AND ALL REQUESTS WITHOUT CQL_FILTERS AND BBOX DIRECTLY TO THE GEOSERVER INSTANCE
+ * (e.g. mod_rewrite in the virtual host, e.g. RewriteCond "%{REQUEST_URI}" "!CQL_FILTER" [NC] combined
+ * with RewriteCond "%{REQUEST_URI}" "!BBOX" [NC], I.E. PROCESS ONLY QUERIES WITH FILTERS IN PHP
  */
 
 /**
@@ -16,7 +18,7 @@
 
 // Define Geoserver base URL and service path for requests
     $geoserverBaseURL = 'http://localhost:8080/geoserver';
-    $geoserverServicePath = '/some/path/ows?'; // preview the layer in geoserver and copy the part of the URL before the ?
+    $geoserverServicePath = '/eposcss/ows?';
     
 // define @epos-style styling parameters according to the documentation of EPOS GeoJSON (to be injected before returning the response)
 // (https://epos-eric.github.io/opensource-docs/documentation/system-reference/data-formats/geojson)
@@ -31,7 +33,7 @@
     
 // Array with the geometry columns (attributes) of the layer, that should be queried.
 // Each database entry will only have one geometry filled, the geometry depending on the type of data set.
-    $geometryColumns = ['geomPoint', 'geomLine', 'geomPolygon'];
+    $geometryColumns = ['geomPoint', 'geomLine', 'geomPolygon', 'geomCollection'];
     //$geometryColumns = ['geomLine']; // single geometry
     
 // Pre-process the incoming URL and preprocess the query
@@ -50,6 +52,8 @@
 
  /**
  * Processes the query string:
+ *   Test for bbox-filter in the main KVP-string.
+ *   If the bbox-filter is there, CQL-filters are not allowed. Convert the bbox-filter to a CQL-filter with all geometries
  *   Remove empty CQL filters
  *   URL-encode the CQL filters
  *   Returns the correct query string for Geoserver.
@@ -62,38 +66,77 @@ function preprocessQuery ($query) {
 
     // separate the CQL filter string
     preg_match('#(.+CQL_FILTER=)(.+)#', $query, $cqlMatches);
-    // remove empty CQL filters
-    $CQL = preg_replace('#\w+=\'\?\'\sAND\s#', '', $cqlMatches[2]);
-    // change request format from "json" (as required by the portal for visibility in map and table view) to
-    // application/json, for correct processing by Geoserver
-    $baseQuery = preg_replace('#outputFormat=json#', 'outputFormat=application/json', $cqlMatches[1]);
-    // reformat comparative and range filters
-    $CQL = convertComparativeAndRangeFilters($CQL);
-    
-    // if only one geometry column is specified, a simple replacement of the bbox geometry-placeholder is sufficient,
-    // else, a more elaborate concatenation is required.
-    if (count($geometryColumns) == 1) {
-        $CQL = str_replace("geometry", $geometryColumns[0], $CQL);
-    } else {
-        // extract bounding box
-        preg_match('#bbox\(geometry,(.+)#', $CQL, $bboxMatches);
-        // repleace the bbox geometry-placeholder with the first geometry and remove the latter from the array
-        $CQL = str_replace("geometry", array_shift($geometryColumns), $CQL);//
-        // open paranthesis before the first bbox-filter
-        $CQL = str_replace("bbox", "(bbox", $CQL);
-        // add bbox-filters for the remaining geometries 
-        foreach ($geometryColumns as $geometryColumn) {
-            $CQL .= " OR bbox(" . $geometryColumn . "," . $bboxMatches[1];
-        }
-        // close paranthesis after the last bbox (end of filter)
-        $CQL .= ")"; 
+    // a switch that triggers CQL-filter processing in case $cqlMatches is not empty, which is the case for all queries from the EPOS portal,
+    // and alternatively allows WFS-queries without CQL-filters, with or without KVP-bounding box
+    switch (true) {
+        case !empty($cqlMatches):
+            // remove empty CQL filters from the EPOS portal
+            $CQL = preg_replace('#\w+=\'\?\'\sAND\s#', '', $cqlMatches[2]);
+            // change request format from "json" (as required by the portal for visibility in map and table view) to
+            // application/json, for correct processing by Geoserver
+            $baseQuery = preg_replace('#outputFormat=json#', 'outputFormat=application/json', $cqlMatches[1]);
+            // reformat comparative and range filters
+            $CQL = convertComparativeAndRangeFilters($CQL);
+            // check whether the query contains a CQL bounding box
+            switch (true) {
+                // if not, do noting
+                case !preg_match("#bbox#i", $CQL):
+                    break;
+                // else, process the bounding box
+                default:
+                    // remove bbox from $CQL, to treat it separately before adding it again - else there is a risk of mixing up attributes in the filter string
+                    $CQL = trim(preg_replace('#(?:AND\s*)?bbox\(geometry,-?\d+,-?\d+,-?\d+,-?\d+,*[\d\w:\']*\)#', '', $CQL), " AND");
+                    // if only one geometry column is specified, a simple replacement of the bbox geometry-placeholder is sufficient,
+                    // else, a more elaborate concatenation is required.
+                    if (count($geometryColumns) == 1) {
+                        $CQL = str_replace("geometry", $geometryColumns[0], $CQL);
+                    } else {
+                        // extract bounding box
+                        preg_match('#bbox\(geometry,(.+)\)#', $cqlMatches[2], $bboxMatches);
+                        // test if $CQL is not empty, if not empty add the AND for the following bbox statement
+                        if (!empty($CQL)) {
+                            $CQL .= " AND ";
+                        }                        
+                        // add first bbox with the first geometry and remove the latter from the array
+                        $CQL .= "(bbox(" . array_shift($geometryColumns). "," . $bboxMatches[1] . ')';
+                        // add bbox-filters for the remaining geometries 
+                        foreach ($geometryColumns as $geometryColumn) {
+                            $CQL .= " OR bbox(" . $geometryColumn . "," . $bboxMatches[1] . ')';
+                        }
+                        // close paranthesis after the last bbox (end of filter)
+                        $CQL .= ")"; 
+                    }
+            }
+            // URL encode the remaining CQL filters
+            $CQL = urlencode($CQL);
+            // assemble query for Geoserver
+            $geoserverQuery = $baseQuery . $CQL;
+            break;
+        default:
+            // detect bbox in the main KVP query
+            preg_match('#(.+)&bbox=([-+]?[0-9]*\.?[0-9]+),([-+]?[0-9]*\.?[0-9]+),([-+]?[0-9]*\.?[0-9]+),([-+]?[0-9]*\.?[0-9]+)#', $query, $bboxCoordinates);
+            switch (true) {
+                case !empty($bboxCoordinates):
+                    // transform KVP bounding box to CQL bounding box
+                    // add first geometry column with filter and remove it fromt he array
+                    $CQL = "(bbox(" . array_shift($geometryColumns) . "," . $bboxCoordinates[2] . "," . $bboxCoordinates[3] . "," . $bboxCoordinates[4] . "," . $bboxCoordinates[5] . ")";
+                    foreach ($geometryColumns as $geometryColumn) {
+                        $CQL .= " OR bbox(" . $geometryColumn . "," . $bboxCoordinates[2] . "," . $bboxCoordinates[3] . "," . $bboxCoordinates[4] . "," . $bboxCoordinates[5] . ")";
+                    }
+                    // close paranthesis after the last bbox (end of filter)
+                    $CQL .= ")"; 
+                    // URL encode the CQL-bounding box filter
+                    $CQL = urlencode($CQL);
+                    // assemble query for Geoserver
+                    $geoserverQuery = $bboxCoordinates[1] . "&CQL_FILTER=" . $CQL;
+                    break;
+                default:
+                    // without any bounding box and CQL-filter parameters, forward the query as it is to Geoserver
+                    $geoserverQuery = $query;
+                    break;
+            }
+            break;
     }
-
-    // URL encode the remaining CQL filters
-    $CQL = urlencode($CQL);
-    // assemble query for Geoserver
-    $geoserverQuery = $baseQuery . $CQL;
-
     return $geoserverQuery;
 }
 
@@ -103,7 +146,7 @@ function preprocessQuery ($query) {
  *   Detects </>/- in the filter parameter, like
  *        property='<XXXX'
  *        property='>XXXX'
- *        property='XXXX-YYYY'
+ *        property='XXXX--YYYY' or property='XXXX to YYYY'
  *   Replaces the input with a correct CQL-filter, like
  *        property<'XXXX'
  *        property>'XXXX'
@@ -123,14 +166,14 @@ function convertComparativeAndRangeFilters($CQL) {
         // ([^']*)        : Match 3 - The value (anything except a quote)
         "/([a-zA-Z0-9]+)='([<>])([^']*)'/",
 
-        // Rule 3: Matches: prop='val1-val2'
+        // Rule 3: Matches: prop='val1--val2'
         // Breakdown:
         // ([a-zA-Z0-9]+) : Match 1 - The property name
         // ='             : Literal characters
-        // ([^'-]+)       : Match 2 - Start value (anything except quote or hyphen)
-        // -              : The hyphen separator
-        // ([^']+)        : Match 3 - End value (anything except quote)
-        "/([a-zA-Z0-9]+)='([^'-]+)-([^']+)'/"
+        // ([^'-+]+)       : Match 2 - Start value (anything except quote or hyphen)
+        // \s*(?:--|to)\s*: The double-hyphen or 'to' separator, surrounded by (space OR +) or not (+ because of URL encoding)
+        // ([^'+]+)        : Match 3 - End value (anything except quote)
+        "/([a-zA-Z0-9]+)='([^' -+]+)(?:\s*|\+*)(?:--|to)(?:\s*|\+*)([^'+]+)'/"
     ];
 
     // Define the corresponding replacements
@@ -196,7 +239,7 @@ function geoserverResponseCheck ($response) {
     try {
         // $response is of type string, test if it contains an array of data encoded as string; if not, throw an exception
         if (!preg_match('#^{"#', $response)) {
-            throw new Exception("EPOS CSS Service: The query is not a valid WFS request. Please check your query carefully against the documentation!", 422);
+            throw new Exception("EPOS CSS Service: The query is not a valid request. Please check your query carefully against the documentation!", 422);
         }
     } catch (Exception $ex) {
         header('Content-Type: text/html');
